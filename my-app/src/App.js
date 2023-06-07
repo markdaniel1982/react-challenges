0d9156b4758a00883a3c737e6ef008eb38631b07
markdaniel1982 <>
2023-06-07 11:28:39 +0000

    add css styling and a fields to Sidebar.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import css from './App.module.css';
+import Sidebar from './components/Sidebar.js';
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      { < Sidebar /> }
     </div>
   );
 }
