ced51f6706fa45e78dc3bd3a6362abf9833aff94
markdaniel1982 <>
2023-06-08 09:19:52 +0000

    add login section to navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
-import css from './App.module.css';
-import Sidebar from './components/Sidebar.js';
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm"
 
 function App() {
   return (
     <div className={css.App}>
-      { < Sidebar /> }
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
     </div>
   );
 }
