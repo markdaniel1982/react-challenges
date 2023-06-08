fb02ec3bc71e0be27498173d4558e76e58f03cb4
markdaniel1982 <>
2023-06-08 09:31:31 +0000

    add content complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
-import NavBarForm from "./components/NavBarForm"
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* Add your components here */}
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
